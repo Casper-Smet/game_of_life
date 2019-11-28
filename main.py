2c416099ba4f0f53f314b56c0230ee1ee1bc0c53
Casper Smet <casper.smet@student.hu.nl>
2019-11-28 16:48:23 +0100

    Added a glider to main.py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ VISUALISATION=True
 
 if __name__ == "__main__":
     w = World(110)
+    glider = input("Y/n")
+    if  glider.capitalize() == "Y":
+        w.set(2, 0)
+        w.set(2, 1)
+        w.set(2, 2)
+        w.set(0, 1)
+        w.set(1, 2)
     sim = Simulator(w)
 
     if VISUALISATION:
